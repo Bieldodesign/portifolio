9c725709ea52909b3f2dc948b347fb4c13c21a46
Gabriel <bieldodesign@gmail.com>
2023-08-30 11:13:29 -0300

    30.08
    links corretos e tratamento das imagens

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-window.addEventListener('scroll', function() {
-    var scrollPosition = window.scrollX || window.pageXOffset;
+ window.addEventListener('scroll', function() {
+     var scrollPosition = window.scrollX || window.pageXOffset;
   
-    var myHeading = document.getElementById('abbout-menu');
+     var myHeading = document.getElementById('abbout-menu');
   
     
-    console.log("scrollPosition")
+     console.log("scrollPosition")
 
-    if (scrollPosition > 200) {
-      myHeading.classList.add('decorated');
-    } else {
-      myHeading.classList.remove('decorated');
-    }
+     if (scrollPosition > 200) {
+       myHeading.classList.add('decorated');
+     } else {
+       myHeading.classList.remove('decorated');
+     }
   });
